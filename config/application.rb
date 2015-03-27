64e87365758ef7c5f956169a4b58d76493e10077
T.Ishii <tomoyasu0620@gmail.com>
2015-03-27 18:47:56 +0900

    config/application.rbの設定

--- config/application.rb
+++ config/application.rb
@@ -15,12 +15,26 @@ module StartDash
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Tokyo'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+
+    # ScaffoldのGenerator設定
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.jbuilder false
+      #g.template_engine false
+      #g.test_framework :rspec, view_specs: false, controller_specs: false, helper_specs: false, request_specs: false, routing_specs: false, fixture: false
+      #g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
+
   end
 end
